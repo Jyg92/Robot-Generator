9a8fac4479efca29204f75bb68fc5758b4910422
Jessica Garcia <jygarciasolis@gmail.com>
2021-11-10 19:42:19 -0500

    set alerts for attacks and enemy and player health after attack

--- assets/js/game.js
+++ assets/js/game.js
@@ -19,9 +19,42 @@ enemyHealth = enemyHealth - playerAttack;
 //Log a resulting message to the console so we know it worked
 console.log( 
     playerName + "attacked" + enemyName + ". " + enemyName + "now has" + enemyHealth + "health remaining.")
+
+// check enemy's health
+if (enemyHealth <=0) {
+    window.alert(enemyName + "has died");
+}
+else{
+    window.alert(enemyName + "still has" + enemyHealth + "health left.");
+}
+
 //Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable
 playerHealth = playerHealth - enemyAttack;
 //Log a resulting message to the console so we know it worked
 console.log(
     enemyName + "attacked" + playerName + ". " + playerName + "now has" + playerHealth + "health remaining.")
 fight(); 
+
+//check player's health
+if (playerHealth <=0) {
+    window.alert(playerName + "has died");
+}
+else {
+    window.alert(playerName + "still has" + playerHealth + "health left.");
+}
+
+// check to see if value of playerHealth variable is greater than 0
+var playerHealth = 100;
+
+if (playerHealth > 0) {
+    console.log("Your player is still alive!");
+}
+
+var playerHealth = 10; 
+
+if (playerHealth === 0) {
+    console.log("This will not run");
+}
+else {
+    console.log("This will run instead");
+}
